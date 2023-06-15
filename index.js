b10f677cf9b192ab8e3be2e67bfbdbd45ed72503
Anshul Rai <41705919+auralshin@users.noreply.github.com>
2023-06-16 03:31:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,54 @@
 'use strict';
 
-var mongoose = require('mongoose');
-var uuidParse = require('uuid-parse');
+const mongoose = require('mongoose');
+const uuidParse = require('uuid-parse');
 
-var Document = mongoose.Document;
+const Document = mongoose.Document;
 
-function getter(binary) {
+const getter = (binary) => {
   if (binary == null) return undefined;
   if (!(binary instanceof mongoose.Types.Buffer)) return binary;
 
-  var len = binary.length;
-  var buf = Buffer.alloc(len);
-  var hex = '';
+  const len = binary.length;
+  const buf = Buffer.alloc(len);
 
-  for (var i = 0; i < len; i++) {
+  let hex = '';
+
+  for (let i = 0; i < len; i++) {
     buf[i] = binary[i];
   }
 
-  for (var i = 0; i < len; i++) {
-    var n = buf.readUInt8(i);
-
-    if (n < 16){
-      hex += '0' + n.toString(16);
-    } else {
-      hex += n.toString(16);
-    }
+  for (let i = 0; i < len; i++) {
+    const n = buf.readUInt8(i);
+    hex += n < 16 ? `0${n.toString(16)}` : n.toString(16);
   }
 
-  return hex.substr(0, 8) + '-' + hex.substr(8, 4) + '-' + hex.substr(12, 4) + '-' + hex.substr(16, 4) + '-' + hex.substr(20, 12);
-}
+  return `${hex.substr(0, 8)}-${hex.substr(8, 4)}-${hex.substr(12, 4)}-${hex.substr(16, 4)}-${hex.substr(20, 12)}`;
+};
 
-function checkRequired(value) {
+const checkRequired = (value) => {
   return value instanceof mongoose.Types.Buffer;
 };
 
-function cast(value, doc, init) {
+const cast = (value, doc, init) => {
   if (value instanceof mongoose.Types.Buffer) return value;
 
   if (typeof value === 'string') {
-    var uuidBuffer = new mongoose.Types.Buffer(uuidParse.parse(value));
-
+    const uuidBuffer = new mongoose.Types.Buffer(uuidParse.parse(value));
     return uuidBuffer.toObject();
   }
 
-  throw new Error('Could not cast ' + value + ' to UUID.');
+  throw new Error(`Could not cast ${value} to UUID.`);
 };
 
-function castForQuery($conditional, val) {
-  var handler;
+const castForQuery = ($conditional, val) => {
+  let handler;
 
   if (arguments.length === 2) {
     handler = this.$conditionalHandlers[$conditional];
 
     if (!handler) {
-      throw new Error("Can't use " + $conditional + " with UUID.");
+      throw new Error(`Can't use ${$conditional} with UUID.`);
     }
 
     return handler.call(this, val);
